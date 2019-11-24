e1fca6a92a3a5d1226e7a9aa1300555727679e37
Tanmay Aggarwal <tanmay.agg@gmail.com>
2019-11-23 22:29:24 -0800

    Function for viewing generated samples

---
+++ view_samples.py
@@ -0,0 +1,16 @@
+# helper function for viewing a list of passed in samples images
+# import standard libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+def view_samples(epoch, samples):
+    fig, axes = plt.subplots(figsize=(16,4), nrows=2, ncols=8, sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), samples[epoch]):
+        img = img.detach().cpu().numpy()
+        img = np.transpose(img, (1, 2, 0))
+        img = ((img +1)*255 / (2)).astype(np.uint8) # rescale to pixel range (0-255)
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        im = ax.imshow(img.reshape((32,32,3)))
+
+    return im
